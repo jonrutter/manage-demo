54156650c9bf79ff0dc6b3d2969bcf761b74aa2f
Jon Rutter <contact@jonrutter.com>
2021-07-24 12:48:22 -0400

    Basic Navbar styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // styles
 import s from './Navbar.module.css';
 
-const Navbar = () => (
-  <div>
-    <h2>Navbar</h2>
-  </div>
-);
+// icon
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+// logo
+import SiteLogo from '../../images/logo.svg';
+
+// data
+import { links } from '../../nav-links';
+
+const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  return (
+    <div className={s.wrapper}>
+      <nav className={s.content}>
+        <img className={s.logo} src={SiteLogo} alt="Manage" />
+        <ul className={s.links}>
+          {links.map((link, index) => {
+            const { text, url } = link;
+            return (
+              <li key={index}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+        <button className={s.ctaBtn}>Get Started</button>
+        <button
+          className={s.toggleBtn}
+          onClick={() => setNavOpen((prev) => !prev)}
+        >
+          {navOpen ? <FaTimes /> : <FaBars />}
+        </button>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
---
+++ src/nav-links.js
@@ -0,0 +1,7 @@
+export const links = [
+  { text: 'Pricing', url: '/' },
+  { text: 'Product', url: '/' },
+  { text: 'About Us', url: '/' },
+  { text: 'Careers', url: '/' },
+  { text: 'Community', url: '/' },
+];
