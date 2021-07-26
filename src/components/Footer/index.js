c44c4355d21d72291191eba92208adb2d6408401
Jon Rutter <contact@jonrutter.com>
2021-07-26 03:13:14 -0400

    Footer form custom validation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // styles
 import s from './Footer.module.css';
@@ -16,10 +16,32 @@ import {
 } from 'react-icons/fa';
 
 const Footer = () => {
+  const [formValue, setFormValue] = useState('');
+  const [formValid, setFormValid] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/;
+    const value = formValue;
+
+    if (regex.test(value)) {
+      setFormValid(true);
+      return;
+    } else {
+      setFormValid(false);
+      return;
+    }
+  };
+
   return (
     <div className={s.wrapper}>
       <footer className={s.content}>
-        <form noValidate className={s.form}>
+        <form
+          noValidate
+          className={formValid ? s.form : `${s.form} ${s.invalid}`}
+          onSubmit={handleSubmit}
+        >
           <label className={s.hiddenLabel} htmlFor="email-signup">
             Sign up for our email mailing list
           </label>
@@ -28,11 +50,14 @@ const Footer = () => {
               type="email"
               placeholder="Updates in your inbox..."
               className={s.input}
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
             />
             <button type="submit" className={s.submit}>
               Go
             </button>
           </div>
+          {!formValid && <p className={s.msg}>Please input a valid email.</p>}
         </form>
         <div className={s.linksWrap}>
           <ul className={s.links}>
