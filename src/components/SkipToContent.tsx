36b5e38544faaa014107fb5aa1984d91ff993cda
Jon Rutter <contact@jonrutter.io>
2023-05-09 02:02:13 -0400

    Update SkipToContent component
    - Migrate to typescript
    
    - Migrate to tailwind

---
+++ src/components/SkipToContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SkipToContent = () => (
+  <a
+    href="#main"
+    className="fixed top-0 left-0 right-0 py-2 px-8 outline-none border-2 border-red bg-lightGrey text-darkBlue text-center  underline -translate-y-full transition-all text-lg focus:translate-y-0"
+  >
+    Skip to content
+  </a>
+);
